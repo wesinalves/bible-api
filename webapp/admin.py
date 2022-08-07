5293ba088283465933d494f4208573ed5293667e
Wesin Ribeiro <wesinec@gmail.com>
2022-08-07 15:47:01 -0300

    add webapp

---
+++ webapp/admin.py
@@ -0,0 +1,77 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Idiom, Book, BookVersion, Chapter, Verse, VerseVersion, VerseDictionary, VerseInterlinear, Reference, Interlinear, Dictionary, Version
+
+
+class BookVersionAdmin(admin.StackedInline):
+    """BookVersion Inline."""
+
+    model = BookVersion
+    extra = 1
+
+
+class InterlinearAdmin(admin.StackedInline):
+    """BookVersion Inline."""
+
+    model = VerseInterlinear
+    extra = 1
+
+class DictionaryAdmin(admin.StackedInline):
+    """BookVersion Inline."""
+
+    model = VerseDictionary
+    extra = 1
+
+class ReferenceAdmin(admin.StackedInline):
+    """BookVersion Inline."""
+
+    model = Reference
+    extra = 1
+    fk_name = 'reference'
+
+
+class BookAdmin(admin.ModelAdmin):
+    """Book Admin."""
+
+    search_fields = ['name']
+    inlines = [
+        BookVersionAdmin,
+    ]
+
+
+class VerseAdmin(admin.ModelAdmin):
+    """Verse Admin."""
+
+    list_filter = ['book']
+    list_display = ['number','chapter']
+    search_fields = ['book', 'chapter', 'number']
+    inlines = [
+        DictionaryAdmin, InterlinearAdmin, ReferenceAdmin
+    ]
+
+class VerseVersionAdmin(admin.ModelAdmin):
+    """BookVersion Inline."""
+
+    list_filter = ['version']
+    list_display = ['verse','version']
+    search_fields = ['verse', 'version', 'text']
+    readonly_fields = ['verse', 'version']
+
+
+class VersionAdmin(admin.ModelAdmin):
+    """Version Admin."""
+
+    list_display = ['name', 'abbreviation']
+
+
+admin.site.register(Idiom)
+admin.site.register(Version, VersionAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Chapter)
+admin.site.register(Verse, VerseAdmin)
+admin.site.register(Interlinear)
+admin.site.register(Dictionary)
+admin.site.register(VerseVersion, VerseVersionAdmin)
+
+#sMw%SQzO$ziJ 0 O
---
+++ webapp/models.py
@@ -0,0 +1,154 @@
+from django.db import models
+
+
+class Idiom(models.Model):
+    """Idiom class."""
+
+    name = models.CharField(max_length=100)
+    native_name = models.CharField(max_length=100)
+    default = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        """Return the name of the Idiom."""
+        return self.name
+
+
+class Version(models.Model):
+    """Version Class."""
+
+    idiom = models.ForeignKey(Idiom, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=20)
+    default = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        """Return the name of the version."""
+        return self.name
+
+
+class Book(models.Model):
+    """Book Class."""
+
+    name = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=5)    
+    chapters = models.IntegerField(default=0)
+    versions = models.ManyToManyField(Version, through='BookVersion')
+
+    def __str__(self) -> str:
+        """Return the name of the book."""
+        return self.name
+
+
+class BookVersion(models.Model):
+    """Book Version realtionship."""
+
+    name = models.CharField(max_length=100)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    version = models.ForeignKey(Version, on_delete=models.CASCADE)    
+
+    def __str__(self) -> str:
+        """Return the name of the book version."""
+        return self.name
+
+
+class Chapter(models.Model):
+    """Chapter Class."""
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    number = models.IntegerField()    
+    verses = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        """Return the number of the chapter."""
+        return f"{self.book.name} cap. {self.number}"
+
+
+class Interlinear(models.Model):
+    """Interlinear Class."""
+
+    strong = models.CharField(max_length=100)
+    definition = models.TextField()
+    origin = models.TextField()
+    use = models.TextField()
+    classification = models.TextField()
+    transcription = models.CharField(max_length=200)
+    pronounce = models.CharField(max_length=200)
+    spelling = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        """Return the reference."""
+        return self.definition
+
+
+class Dictionary(models.Model):
+    """Dictionary Class."""
+
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        """Return the dictionary."""
+        return self.title
+
+
+class Verse(models.Model):
+    """Verse Class."""
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)    
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    number = models.IntegerField()        
+    versions = models.ManyToManyField(Version, through='VerseVersion')
+    dictionaries = models.ManyToManyField(Dictionary, through='VerseDictionary', null=True, blank=True)
+    intelinears = models.ManyToManyField(Interlinear, through='VerseInterlinear', null=True, blank=True)    
+
+    def __str__(self) -> str:
+        """Return the number of the verse."""
+        return f"{self.book.abbreviation} {self.chapter.number}:{self.number}"
+
+
+class VerseInterlinear(models.Model):
+    """Verse Interlinear Relationship."""
+
+    verse = models.ForeignKey(Verse, on_delete=models.CASCADE)
+    interlinear = models.ForeignKey(Interlinear, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        """Return the number of the verse."""
+        return f'{self.verse.book.abbreviation} {self.verse.chapter.number}:{self.verse.number}'
+
+
+class VerseDictionary(models.Model):
+    """Verse Dicitionary Relationship."""
+
+    verse = models.ForeignKey(Verse, on_delete=models.CASCADE)
+    dictionary = models.ForeignKey(Dictionary, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        """Return the number of the verse."""
+        return f'{self.verse.book.abbreviation} {self.verse.chapter.number}:{self.verse.number}'
+
+
+class VerseVersion(models.Model):
+    """Verse Version Relationship."""
+
+    verse = models.ForeignKey(Verse, on_delete=models.CASCADE)
+    version = models.ForeignKey(Version, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        """Return the number of the verse."""
+        return f'{self.verse.book.abbreviation} {self.verse.chapter.number}:{self.verse.number}'
+
+
+class Reference(models.Model):
+    """Reference Class."""
+
+    reference = models.ForeignKey(Verse, on_delete=models.CASCADE, related_name='verse_fk')
+    text = models.CharField(max_length=100)
+    book = models.CharField(max_length=5)
+    chapter = models.IntegerField()
+    verse = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        """Return the reference."""
+        return self.text
