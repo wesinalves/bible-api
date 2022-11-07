29580540a25351c3b531c7fa55f4279860733bec
Wesin Alves <wesinec@gmail.com>
2022-11-07 12:10:17 -0300

    update version url

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('versions', views.versions, name='versions'),
+    path('', views.index, name='index'),    
+    path('versions/<str:version_abbr>', views.versions, name='versions'),
     path('books', views.books, name='books'),
     path('<str:version_abbr>/<str:book_abbr>', views.chapters, name='chapters'),
     path('<str:version_abbr>/<str:book_abbr>/<int:chapter_number>', views.verses, name='verses'),
