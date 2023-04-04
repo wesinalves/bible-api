25b22d8e0668de63f5e901437090624081fcdc71
Wesin Ribeiro Alves <wesinec@gmail.com>
2023-04-03 22:20:02 -0300

    add order models

--- webapp/models.py
+++ webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 
 class Idiom(models.Model):
@@ -152,3 +153,16 @@ class Reference(models.Model):
     def __str__(self) -> str:
         """Return the reference."""
         return self.text
+
+class Order(models.Model):
+    secret = models.CharField(max_length=1000, default="", blank=True)
+    amount = models.DecimalField(default=0)
+    paid = models.BooleanField(default=False)
+    checkout_url = models.CharField(max_length=1000, default="", blank=True)
+
+    def __str__(self) -> str:
+        """Return the order chekout"""
+        return self.checkout_url
+    
+    def generate_secret(self):
+        self.secret = str(random.randint(10000, 99999))
