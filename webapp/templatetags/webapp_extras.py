02f2450279c4afe301eba66dfc440062f87d3e0c
Wesin Ribeiro Alves <wesinec@gmail.com>
2023-04-19 06:55:31 -0300

    add multiply tag

---
+++ webapp/templatetags/webapp_extras.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def multiply(qty, unit_price):
+    # you would need to do any localization of the result here
+    return qty * unit_price
